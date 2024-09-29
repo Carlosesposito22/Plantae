cfa228c624d1f00c1f62a4e508092933a2b766e4
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2024-09-29 11:16:53 -0300

    Mudei os nomes dos arquivos

--- plantae/settings.py
+++ plantae/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bookheaded.urls'
+ROOT_URLCONF = 'plantae.urls'
 
 TEMPLATES = [
     {
@@ -57,7 +57,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bookheaded.wsgi.application'
+WSGI_APPLICATION = 'plantae.wsgi.application'
 
 
 # Database
@@ -93,9 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
